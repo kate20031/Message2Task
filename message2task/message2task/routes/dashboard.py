17ec52ec9c365d84714e0ac7e0d9b8f91cba2ea2
kate20031 <83357098+kate20031@users.noreply.github.com>
2025-06-02 19:37:35 +0300

    Create dashboard.py

---
+++ message2task/message2task/routes/dashboard.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, session, render_template, redirect, url_for, flash
+from models import Message
+
+dashboard_bp = Blueprint('dashboard', __name__)
+
+@dashboard_bp.route("/dashboard")
+def dashboard():
+    if 'user_id' not in session:
+        flash('You need to log in first.', 'warning')
+        return redirect(url_for('login'))
+
+    messages = Message.query.filter_by(user_id=session['user_id']).order_by(Message.date.desc()).limit(10).all()
+
+    message_list = [{
+        'from': msg.from_user,
+        'ai_task': msg.ai_task,
+        'date': msg.date
+    } for msg in messages]
+
+    return render_template('dashboard.html', username=session['user'], messages=message_list)
