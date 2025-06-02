3852cdf999427f4f526a02cbeca27fc3a026ff85
kate20031 <83357098+kate20031@users.noreply.github.com>
2025-06-02 19:59:40 +0300

    Update routes.py

--- message2task/message2task/routes.py
+++ message2task/message2task/routes.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, request, jsonify
-from models import Message
-from . import db
+# from flask import Blueprint, request, jsonify
+# from models import Message
+# from . import db
 
-bp = Blueprint('main', __name__)
+# bp = Blueprint('main', __name__)
 
-@bp.route('/add_message', methods=['POST'])
-def add_message():
-    data = request.json
-    new_message = Message(content=data['content'])
-    db.session.add(new_message)
-    db.session.commit()
-    return jsonify({'message': 'Message saved successfully!'})
+# @bp.route('/add_message', methods=['POST'])
+# def add_message():
+#     data = request.json
+#     new_message = Message(content=data['content'])
+#     db.session.add(new_message)
+#     db.session.commit()
+#     return jsonify({'message': 'Message saved successfully!'})
