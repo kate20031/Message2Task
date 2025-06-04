7244d880c914eb66d522739173fb32b8a744b260
kate20031 <83357098+kate20031@users.noreply.github.com>
2025-06-04 11:25:55 +0300

    Update __init__.py

--- message2task/message2task/routes/__init__.py
+++ message2task/message2task/routes/__init__.py
@@ -1,4 +1,4 @@
 from .dashboard import bp as dashboard_bp
 from .auth import bp as auth_bp
 from .messages import messages_bp
-from .webhook import bp as webhook_bp
+from .messages import bp as webhook_bp
