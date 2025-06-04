fa0663194d739059c6538c8ddd2becfc8e7b8b1e
kate20031 <83357098+kate20031@users.noreply.github.com>
2025-06-04 12:04:51 +0300

    Update __init__.py

--- message2task/message2task/routes/__init__.py
+++ message2task/message2task/routes/__init__.py
@@ -1,7 +1,8 @@
 from .dashboard import dashboard_bp
 from .auth import auth_bp
 from .messages import messages_bp
-
+from .home import home_bp 
 
 # Експортуй для create_app
-__all__ = ["dashboard_bp", "auth_bp", "messages_bp", "webhook_bp"]
+__all__ = ["dashboard_bp", "auth_bp", "messages_bp", "home_bp"]
+
