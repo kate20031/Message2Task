4273cb99bf24c7b14f61d50c214b26d63d6c0db4
kate20031 <83357098+kate20031@users.noreply.github.com>
2025-06-07 19:02:24 +0300

    Update time_utils.py

--- message2task/message2task/utils/time_utils.py
+++ message2task/message2task/utils/time_utils.py
@@ -7,14 +7,12 @@ def normalize_time(raw_time):
     if not raw_time:
         return None
 
-    # Замінюємо крапку на двокрапку
     fixed_time = raw_time.replace('.', ':')
 
-    # Розбиваємо на години і хвилини
     match = re.match(r'^(\d{1,2}):(\d{1,2})$', fixed_time)
     if match:
         hours = int(match.group(1))
         minutes = int(match.group(2))
-        return f"{hours:02d}:{minutes:02d}"  # Формат: HH:MM
+        return f"{hours:02d}:{minutes:02d}"  # HH:MM
     else:
         return None
