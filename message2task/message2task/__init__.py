6266411693a608e859189b0e4da84059bce56ab1
kate20031 <83357098+kate20031@users.noreply.github.com>
2025-06-07 15:10:40 +0300

    Update __init__.py

--- message2task/message2task/__init__.py
+++ message2task/message2task/__init__.py
@@ -46,13 +46,58 @@
 #     return app
 
 
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from dotenv import load_dotenv
+# import os
+# import logging
+
+
+# db = SQLAlchemy()
+# migrate = Migrate()
+
+# def create_app():
+#     load_dotenv()
+
+#     app = Flask(__name__)
+#     app.secret_key = os.getenv('SECRET_KEY', os.urandom(24))
+#     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#     db.init_app(app)
+#     migrate.init_app(app, db)
+    
+#     from .routes import dashboard_bp, auth_bp, messages_bp, home_bp
+
+#     app.register_blueprint(dashboard_bp)
+#     app.register_blueprint(auth_bp)
+#     app.register_blueprint(messages_bp)
+#     app.register_blueprint(home_bp)
+#     print(app.url_map) 
+
+
+    
+#     logging.basicConfig(level=logging.INFO)
+#     logger = logging.getLogger(__name__)
+    
+#     @app.errorhandler(500)
+#     def internal_error(e):
+#         app.logger.error('Internal Server Error: %s', e, exc_info=True)
+#         return "500 Internal Server Error", 500
+
+#     return app
+
+
+
+import os
+import logging
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from dotenv import load_dotenv
-import os
-import logging
 
+logging.basicConfig(level=logging.INFO)  # configure once here
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -76,11 +121,6 @@ def create_app():
     app.register_blueprint(home_bp)
     print(app.url_map) 
 
-
-    
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    
     @app.errorhandler(500)
     def internal_error(e):
         app.logger.error('Internal Server Error: %s', e, exc_info=True)
